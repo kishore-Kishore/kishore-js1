52a81e6d7925aad5ba5c32d9760a26ff81679d57
Kishoremahendran <m.kishoremahen123@gmail.com>
2024-09-10 18:55:40 +0530

    initial commit

---
+++ 04-09-24-1-5.js
@@ -0,0 +1,29 @@
+let day =4;
+switch (day){
+    case 1:
+        console.log("today is monday");
+        break;
+    case 2:
+        console.log("today is tuesday");
+        break;
+    case 3:
+        console.log("today is wednesday");
+        break;
+    case 4:
+        console.log("today is thursday");
+        break;
+    case 5:
+        console.log("today is friday");
+        break;
+    case 6:
+        console.log("today is saturday");
+        break;
+    case 7:
+        console.log("today is sunday");
+        break;
+    default:
+        console.log("Invalid day");
+        break;
+}
+ 
+  console.log("To day is your Birthday");
