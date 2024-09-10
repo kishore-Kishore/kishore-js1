8ff8a3ded2e0667faa803c89346fed9da05df60b
kishore-kishore <kishorem4677@gmail.com>
2024-09-10 19:52:22 +0530

    Imitial push

---
+++ switchcase-pract.js
@@ -0,0 +1,12 @@
+// let age=12;
+
+// switch(age){
+//     case (age 5<=):
+//         console.log("free");
+//          break;
+//     case (age 12>):
+//         console.log("print 10");
+//         break;
+//     default:
+//         console.log("not allowed");
+// }
